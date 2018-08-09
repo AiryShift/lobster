46100cec8e15cfb1c451a2364d145253735cf27b
AiryShift <religionofaperturelabs@gmail.com>
2018-08-09 21:52:30 +1000

    Let's get a website up at least

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+if [ -z "$PS1" ]; then echo -e "This script must be sourced." ; exit ; fi
+source venv/bin/activate
+if [[ "$1" == "init" ]]; then
+    pip install -r requirements.txt
+else
+    echo "Did not update dependencies, run with \`source start.sh init\` to update."
+fi
+export FLASK_APP="app.py"
+export FLASK_ENV=development
+flask run
